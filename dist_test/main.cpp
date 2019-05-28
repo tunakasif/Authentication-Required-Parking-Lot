0a92b2ed73092c4e1562dbddd621c50505c85023
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2019-05-29 00:05:09 +0300

    Refactor dist_test

--- dist_test/main.cpp
+++ dist_test/main.cpp
@@ -1,22 +1,21 @@
 #include "mbed.h"
 #include "hcsr04.h"
 #include "TextLCD.h"
- 
-DigitalOut myled(LED1);
-HCSR04  usensor(PTA12, PTD4);
+
+HCSR04 sensor(PTA12, PTD4);
 TextLCD lcd(PTE20, PTE21, PTE22, PTE23, PTE29, PTE30, TextLCD::LCD16x2);
 unsigned int dist;
 
 int main()
 {
     lcd.cls();
-    lcd.printf("Tuna");
-    while(1) {
-        usensor.start();
-        wait_ms(500); 
-        dist=usensor.get_dist_cm();
+    while (true)
+    {
+        sensor.start();
+        wait_ms(500);
+        dist = sensor.get_dist_cm();
         lcd.cls();
-        lcd.locate(0,0);
+        lcd.locate(0, 0);
         lcd.printf("cm: %ld", dist);
     }
 }
