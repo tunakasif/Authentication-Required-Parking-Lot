3f937dc203ba72614a3d1ee8ec8cd85e3bd40e22
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2019-05-29 00:01:10 +0300

    Updated hc-sr04 library

--- dist_test/HCSR04/HCSR04.cpp
+++ dist_test/HCSR04/HCSR04.cpp
@@ -1,81 +1,98 @@
-#include "mbed.h"
-#include "HCSR04.h"
+/* Copyright (c) 2013 Prabhu Desai
+ * pdtechworld@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-HCSR04::HCSR04(PinName echoPin, PinName triggerPin) : echo(echoPin), trigger(triggerPin) {
-    init();
+
+#include "hcsr04.h"
+
+
+HCSR04::HCSR04(PinName TrigPin,PinName EchoPin):
+    trigger(TrigPin), echo(EchoPin)
+{
+    pulsetime.stop();
+    pulsetime.reset();
+    echo.rise(this,&HCSR04::isr_rise);
+    echo.fall(this,&HCSR04::isr_fall);
+    trigger=0;
 }
 
-void HCSR04::init() {
-    distance = -1;      // initial distance
-    minDistance = 2;
-    maxDistance = 400;
-    newDataReady = timerStarted = false;
+HCSR04::~HCSR04()
+{
 }
 
-void HCSR04::startTimer() {
-    if (!timerStarted) {
-        timer.start(); // start the timer
-        timerStarted = true;
-        echoTimeout.attach_us(this, &HCSR04::stopTimer, 25000); // in case echo fall does not occur
-        echo.fall(this, &HCSR04::stopTimer);
-        echo.rise(NULL);
-    }
+void HCSR04::isr_rise(void)
+{
+    pulsetime.start();
+}
+void HCSR04::start(void)
+{
+    trigger=1;
+    wait_us(10);
+    trigger=0;
 }
 
-void HCSR04::stopTimer() {
-    timer.stop(); // stop the timer
-    if (timerStarted) {
-        distance = timer.read() * 1e6 / 58;
-        if (distance < minDistance)
-            distance = minDistance;
-        if (distance > maxDistance)
-            distance = maxDistance;
-        newDataReady = true;
-    }
-    timer.reset();
-    timerStarted = false;
-    echoTimeout.detach();
-    echo.fall(NULL);
+void HCSR04::isr_fall(void)
+{
+    pulsetime.stop();
+    pulsedur = pulsetime.read_us();
+    distance= (pulsedur*343)/20000;
+    pulsetime.reset();
 }
 
-void HCSR04::turnOffTrigger() {
-    trigger = 0; 
+void HCSR04::rise (void (*fptr)(void))
+{
+    echo.rise(fptr);
+}
+void HCSR04::fall (void (*fptr)(void))
+{
+    echo.fall(fptr);
 }
 
-void HCSR04::startMeasurement() {
-    trigger = 1;
-    triggerTimeout.attach_us(this, &HCSR04::turnOffTrigger, 10);
-    echo.rise(this, &HCSR04::startTimer);
-    newDataReady = false;
-}
-
-float HCSR04::getDistance_cm() {
-    newDataReady = false;
+unsigned int HCSR04::get_dist_cm()
+{
     return distance;
 }
-
-float HCSR04::getDistance_mm() {
-    newDataReady = false;
-    return distance * 10;
+unsigned int HCSR04::get_pulse_us()
+{
+    return pulsedur;
 }
 
-bool HCSR04::isNewDataReady() {
-    return newDataReady;
-}
 
-void HCSR04::setRanges(float minRange, float maxRange) {
-    if (minRange < maxRange) {
-        if (minRange >= 2 && minRange < 400) // bug from revs. 4 and 5 corrected
-            minDistance = minRange;
-        if (maxRange <= 400)
-            maxDistance = maxRange;
+
+/*******************************************************
+   Here is a sample code usage
+********************************************************* 
+#include "hcsr04.h"
+HCSR04  usensor(p25,p6);
+int main()
+{
+    unsigned char count=0;
+    while(1) {
+        usensor.start();
+        wait_ms(500); 
+        dist=usensor.get_dist_cm();
+        lcd.cls();
+        lcd.locate(0,0);
+        lcd.printf("cm:%ld",dist );
+ 
+        count++;
+        lcd.locate(0,1);
+        lcd.printf("Distance =%d",count);
+        
     }
-}
-
-float HCSR04::getMinRange() {
-    return minDistance;
-}
-
-float HCSR04::getMaxRange() {
-    return maxDistance;
-}
+*/
--- dist_test/HCSR04/HCSR04.h
+++ dist_test/HCSR04/HCSR04.h
@@ -1,103 +1,68 @@
-#ifndef HCSR04_H_TVZMT
-#define HCSR04_H_TVZMT
-
-/** A distance measurement class using ultrasonic sensor HC-SR04.
- *  
- * Example of use:
- * @code
- * #include "mbed.h"
- * #include "HCSR04.h"
+/* Copyright (c) 2013 Prabhu Desai
+ * pdtechworld@gmail.com
  *
- * Serial pc(USBTX, USBRX);
- * Timer timer;
  *
- * int main() {
- *     HCSR04 sensor(p5, p7);
- *     sensor.setRanges(10, 110);
- *     pc.printf("Min. range = %g cm\n\rMax. range = %g cm\n\r",
- *       sensor.getMinRange(), sensor.getMaxRange());
- *     while(true) {
- *         timer.reset();
- *         timer.start();
- *         sensor.startMeasurement();
- *         while(!sensor.isNewDataReady()) {
- *             // wait for new data
- *             // waiting time depends on the distance
- *         }
- *         pc.printf("Distance: %5.1f mm\r", sensor.getDistance_mm());
- *         timer.stop();
- *         wait_ms(500 - timer.read_ms()); // time the loop
- *     }
- * }
- * @endcode
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * For more details on the sensor :
+ * http://www.elecfreaks.com/store/hcsr04-ultrasonic-sensor-distance-measuring-module-p-91.html?zenid=pgm8pgnvaodbe36dibq5s1soi3
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-class HCSR04 {
-    
-    public:
-    
-    /** Receives two PinName variables.
-     * @param echoPin mbed pin to which the echo signal is connected to
-     * @param triggerPin mbed pin to which the trigger signal is connected to
-     */
-    HCSR04(PinName echoPin, PinName triggerPin);
-    
-    /** Start the measurement. Measurement time depends on the distance.
-     *  Maximum measurement time is limited to 25 ms (400 cm).
-     */
-    void startMeasurement();
-    
-    /** Returns the distance in cm. Requires previous call of startMeasurement().
-     * @returns distance of the measuring object in cm.
-     */
-    float getDistance_cm();
-    
-    /** Returns the distance in mm. Requires previous call of startMeasurement().
-     * @returns distance of the measuring object in mm.
-     */
-    float getDistance_mm();
-    
-    /** Sets the minimum and maximum ranges between the factory values of 2 cm and 400 cm.
-     *  @param minRange Minimum range in cm. Must be between 2 cm and maxRange.
-     *  @param maxRange Maximum range in cm. Must be between minRange and 400 cm.
-     */
-    void setRanges(float minRange, float maxRange);
-    
-    /** Retreives the minimum sensor range set by the user.
-     * @returns the minimum sensor range set by the user in cm.
-     */
-    float getMinRange();
-    
-    /** Retreives the maximum sensor range set by the user.
-     * @returns the maximum sensor range set by the user in cm.
-     */
-    float getMaxRange();
-    
-    /** Checks if the new data is ready.
-     * @returns true if new data is ready, false otherwise.
-     */
-    bool isNewDataReady();
-    
-    private:
-    
-    InterruptIn echo;       // echo pin
-    DigitalOut trigger;     // trigger pin
-    Timer timer;            // echo pulsewidth measurement
-    float distance;         // store the distance in cm
-    float minDistance;      // minimum measurable distance
-    float maxDistance;      // maximum measurable distance
-    Timeout triggerTimeout, echoTimeout;
-    bool newDataReady, timerStarted;
-    
-    /** Start the timer. */
-    void startTimer();
-    
-    /** Stop the timer. */
-    void stopTimer();
-    
-    /** Initialization. */
-    void init();
-    
-    void turnOffTrigger();
+
+#ifndef MBED_HCSR04_H
+#define MBED_HCSR04_H
+
+#include "mbed.h"
+
+/** HCSR04 Class(es)
+ */
+
+class HCSR04
+{
+public:
+    /** Create a HCSR04 object connected to the specified pin
+    * @param pin i/o pin to connect to
+    */
+    HCSR04(PinName TrigPin,PinName EchoPin);
+    ~HCSR04();
+
+    /** Return the distance from obstacle in cm
+    * @param distance in cms and returns -1, in case of failure
+    */
+    unsigned int get_dist_cm(void);
+    /** Return the pulse duration equal to sonic waves travelling to obstacle and back to receiver.
+    * @param pulse duration in microseconds.
+    */
+    unsigned int get_pulse_us(void);
+    /** Generates the trigger pulse of 10us on the trigger PIN.
+    */
+    void start(void );
+    void isr_rise(void);
+    void isr_fall(void);
+    void fall (void (*fptr)(void));
+    void rise (void (*fptr)(void));
+
+
+
+private:
+
+    Timer pulsetime;
+    DigitalOut  trigger;
+    InterruptIn echo;
+    unsigned int pulsedur;
+    unsigned int distance;
 };
 
 #endif
