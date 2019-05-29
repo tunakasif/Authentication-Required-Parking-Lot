e9cc6e3ac2810711ab06a60cea27e7c989768e12
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2019-05-29 23:23:38 +0300

    Add flying fish implementation
    MH Sensor Flying Fish Implementation has been done in order to be used
    in car counting part of the parking lot project. Interrupt driven design
    has been used since it is not known when the car will exit the parking
    lot. ISR of flying fish sensor is carefully implemented due to the fact
    that even though it is defined as rising edge triggerred, it enters the
    ISR in the falling edge as well. Prevented that issue.

--- fish_test/main.cpp
+++ fish_test/main.cpp
@@ -1,10 +1,45 @@
 #include "mbed.h"
+#include "TextLCD.h"
 
-#define FISH PTA5
+TextLCD lcd(PTE20, PTE21, PTE22, PTE23, PTE29, PTE30,
+            TextLCD::LCD16x2);
+DigitalIn fish(PTA5);
+InterruptIn fish_sensor(PTA5);
+DigitalIn fish_pin(PTA5);
 
-DigitalOut myled(LED1);
+// global constants
+const int NUMBER_OF_PARK_SPOTS = 5;
 
-int main() {
-    while(1) {
+// global variables
+int avaliableSpots;
+
+void ISR()
+{
+    // since the flying fish can bounce this wait and
+    // pin check makes sure that it is a rising edge
+    wait(0.1);
+    if (fish_pin.read() == 1)
+    {
+        if (avaliableSpots < NUMBER_OF_PARK_SPOTS)
+        {
+            avaliableSpots++;
+        }
+    }
+}
+
+int main()
+{
+    // program code
+    // Initialize
+    avaliableSpots = NUMBER_OF_PARK_SPOTS;
+    avaliableSpots = 3;
+    fish_sensor.rise(&ISR);
+
+    while (true)
+    {
+        lcd.cls();
+        lcd.locate(0, 0);
+        lcd.printf("%d", avaliableSpots);
+        wait(1);
     }
 }
